89085876b95283403811d08124f22f33a103a5e4
noah-y <dongdhy@naver.com>
2024-10-26 15:10:44 +0900

    fix: change RootStackParamList from interface to type
    - error: does not satisfy the constraint 'ParamListBase'. Index signature for type 'string' is missing in type
    - solution: https://stackoverflow.com/a/71394297

--- src/screens/CategoryScreen.tsx
+++ src/screens/CategoryScreen.tsx
@@ -1,7 +1,15 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Text } from "react-native";
+import { RootStackParamList, ScreenName } from "../types/Screen";
 
-const CategoryScreen = () => {
-  return <Text>Category</Text>;
+type Props = NativeStackScreenProps<RootStackParamList, ScreenName.Category>;
+
+const CategoryScreen = ({
+  route: {
+    params: { category },
+  },
+}: Props) => {
+  return <Text>{category}</Text>;
 };
 
 export default CategoryScreen;
