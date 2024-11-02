ae61c75454371323bafafb773d13621558dc53c4
noah-y <dongdhy@naver.com>
2024-11-02 15:01:37 +0900

    env: add tanstack for image fetching and caching

--- App.tsx
+++ App.tsx
@@ -28,53 +28,57 @@ import {
 } from "react-native/Libraries/NewAppScreen";
 import theme from "./src/styles/theme";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CategoriesScreen from "./src/screens/CategoriesScreen";
 import { RootStackParamList, ScreenName } from "./src/types/Screen";
 import CategoryScreen from "./src/screens/CategoryScreen";
 import TodaysPhraseScreen from "./src/screens/TodaysPhraseScreen";
-import { Category } from "./src/types/Category";
 import GalleryScreen from "./src/screens/GalleryScreen";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   const [checkedTodaysPhrase, setCheckedTodaysPhrase] = useState(false);
 
   return (
-    <NavigationContainer>
-      <ThemeProvider theme={theme}>
-        <SafeAreaView
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          {checkedTodaysPhrase ? (
-            <Stack.Navigator initialRouteName={ScreenName.Categories}>
-              <Stack.Screen
-                name={ScreenName.Categories}
-                component={CategoriesScreen}
-                options={{ title: "카테고리" }}
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <ThemeProvider theme={theme}>
+          <SafeAreaView
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            {checkedTodaysPhrase ? (
+              <Stack.Navigator initialRouteName={ScreenName.Categories}>
+                <Stack.Screen
+                  name={ScreenName.Categories}
+                  component={CategoriesScreen}
+                  options={{ title: "카테고리" }}
+                />
+                <Stack.Screen
+                  name={ScreenName.Category}
+                  component={CategoryScreen}
+                  options={({ route }) => ({ title: route.params.category })}
+                />
+                <Stack.Screen
+                  name={ScreenName.Gallery}
+                  component={GalleryScreen}
+                  options={{ headerShown: false }}
+                />
+              </Stack.Navigator>
+            ) : (
+              <TodaysPhraseScreen
+                handleClose={() => setCheckedTodaysPhrase(true)}
               />
-              <Stack.Screen
-                name={ScreenName.Category}
-                component={CategoryScreen}
-                options={({ route }) => ({ title: route.params.category })}
-              />
-              <Stack.Screen
-                name={ScreenName.Gallery}
-                component={GalleryScreen}
-                options={{ headerShown: false }}
-              />
-            </Stack.Navigator>
-          ) : (
-            <TodaysPhraseScreen
-              handleClose={() => setCheckedTodaysPhrase(true)}
-            />
-          )}
-        </SafeAreaView>
-      </ThemeProvider>
-    </NavigationContainer>
+            )}
+          </SafeAreaView>
+        </ThemeProvider>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
