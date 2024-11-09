ae35fedd9fa36cb8f7f277655be965b19a68a73a
noah-y <dongdhy@naver.com>
2024-11-09 22:35:56 +0900

    feat: add skeleton ui to category screen

--- src/components/PhraseImageButton/index.tsx
+++ src/components/PhraseImageButton/index.tsx
@@ -1,21 +1,11 @@
-import styled, { css } from "@emotion/native/";
+import styled from "@emotion/native/";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { memo, useEffect, useState } from "react";
+import { memo, useState } from "react";
 import { RootRouteProps, ScreenName } from "../../types/Screen";
-import SkeletonBox from "../SkeletonBox";
-import { Image } from "react-native";
-
-const ContainerBaseCss = css`
-  aspect-ratio: 160 / 224;
-  flex: 0.5;
-`;
-
-const SkeletonContainer = styled(SkeletonBox)`
-  ${ContainerBaseCss}
-`;
 
 const Container = styled.TouchableOpacity`
-  ${ContainerBaseCss}
+  aspect-ratio: 160 / 224;
+  flex: 0.5;
   border: 1px solid rgba(17, 17, 17, 0.08);
   border-radius: 16px;
   overflow: hidden;
@@ -44,21 +34,14 @@ const PhraseImageButton = ({ imageUrl }: Props) => {
     });
   };
 
-  useEffect(() => {
-    Image.prefetch(imageUrl).finally(() => {
-      setIsLoading(false);
-    });
-  }, [imageUrl]);
-
-  if (isLoading) {
-    return <SkeletonContainer />;
-  }
-
   return (
     <Container onPress={onPress} disabled={isLoading}>
       <PhraseImage
         source={{ uri: imageUrl }}
-        // onLoadEnd={() => setIsLoading(false)}
+        onLoadEnd={() => setIsLoading(false)}
+        style={{
+          backgroundColor: "#EEEEEE",
+        }}
       />
     </Container>
   );
