d3eaa2dee9eb7e5c5592d59587dc7e93abdbe1e3
noah-y <dongdhy@naver.com>
2024-11-09 20:38:00 +0900

    feat: add loading indicator in download button

--- src/components/PhraseImageCtaButton/PhraseImageDownloadButton.tsx
+++ src/components/PhraseImageCtaButton/PhraseImageDownloadButton.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { downloadImageFromUrl } from "../../utils/file";
 import { PhraseImageCtaButton, PhraseImageCtaButtonText } from "./styles";
+import { ActivityIndicator } from "react-native";
 
 interface Props {
   imageUrl: string;
 }
 
 const PhraseImageDownloadButton = ({ imageUrl }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlePress = async () => {
+    try {
+      setIsLoading(true);
+      await downloadImageFromUrl(imageUrl);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <PhraseImageCtaButton onPress={() => downloadImageFromUrl(imageUrl)}>
-      <PhraseImageCtaButtonText>다운로드</PhraseImageCtaButtonText>
+    <PhraseImageCtaButton onPress={handlePress} disabled={isLoading}>
+      {isLoading ? (
+        <ActivityIndicator size={24} color={"#000000"} />
+      ) : (
+        <PhraseImageCtaButtonText>다운로드</PhraseImageCtaButtonText>
+      )}
     </PhraseImageCtaButton>
   );
 };
