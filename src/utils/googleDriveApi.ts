67b82e50974b9d925c875421f89ea05ea0eebab0
noah-y <dongdhy@naver.com>
2024-11-16 22:09:03 +0900

    feat: connect server for getting google drive access token

--- src/utils/googleDriveApi.ts
+++ src/utils/googleDriveApi.ts
@@ -1,8 +1,22 @@
 import { GDrive } from "@robinbobin/react-native-google-drive-api-wrapper";
+import axios from "axios";
+import Config from "react-native-config";
 
 const googleDrive = new GDrive();
 
 export const signInToGoogle = async () => {
+  try {
+    if (!googleDrive.accessToken) {
+      const result = await axios.get(Config.API_URL as string, {
+        headers: {
+          "aroundthistime-custom-header": Config.CUSTOM_HEADER,
+        },
+      });
+      googleDrive.accessToken = result.data.token;
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const getGoogleDrive = async () => {
